603fcc876e4acb0458a2869903e0409d80e6ebfd
hiepvo01 <vohi0311@gmail.com>
2024-05-02 11:47:33 +1000

    google cloud run

--- webapp/main.js
+++ webapp/main.js
@@ -1,7 +1,7 @@
 async function login(event) {
     event.preventDefault();
     const formData = new FormData(document.getElementById('login-form'));
-    const response = await fetch('https://test-etl-1-0-0.onrender.com/token', {
+    const response = await fetch('https://test-etl-bi5vbfxerq-ts.a.run.app/token', {
         method: 'POST',
         body: formData
     });
@@ -65,7 +65,7 @@ async function displayData(token) {
         }
         
         // const datain = await fetchData(`https://etl-tutorial.onrender.com${dataUrl}`, token);
-        const datain = await fetchData(`https://test-etl-1-0-0.onrender.com${dataUrl}`, token);
+        const datain = await fetchData(`https://test-etl-bi5vbfxerq-ts.a.run.app${dataUrl}`, token);
         document.getElementById('message-display').innerText = "Logged in as " + user
 
         try {
